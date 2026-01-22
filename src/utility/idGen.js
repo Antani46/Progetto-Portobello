aab218d16d136744d0e50ccdaead197e43e1ca26
Antani46 <cerasoli.94@gmail.com>
2026-01-22 13:57:15 +0100

    modifiche finali

--- src/utility/idGen.js
+++ src/utility/idGen.js
@@ -1,11 +1,17 @@
-//Genera un ID univoco di 8 caratteri alfanumerici e la data corrente.
+//Genera un ID univoco di 8 caratteri alfanumerici.
 export const generateUniqueId = () => {
     return Math.random().toString(36).slice(2, 11);
 };
 
+//Genera la data corrente
 export const generateProductMeta = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+
     return {
         id: generateUniqueId(),
-        createdAt: new Date().toISOString()
+        createdAt: `${year}-${month}-${day}`
     };
 };
